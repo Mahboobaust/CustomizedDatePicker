ed5c5f1302a282620a050549398ee7ea32b2fa4f
Mahboobaust <mahboobaust13@gmail.com>
2015-08-30 13:38:31 +0600

    Initial Commit

---
+++ CustomizedDatePicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomizedDatePicker
+//
+//  Created by Mahboob on 8/30/15.
+//  Copyright (c) 2015 Mahboob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomizedDatePicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomizedDatePicker
+//
+//  Created by Mahboob on 8/30/15.
+//  Copyright (c) 2015 Mahboob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
